431c62382145212b56a92e06d3657782253c9ecd
viralsinha12 <viral.sinha@hotmail.com>
2019-05-08 08:56:13 -0400

    Register Airlines Dynamically,Loading Airlines from the dropdown;color change on status

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3')
-const web3 = new Web3('http://127.0.0.1:8545')
+const web3 = new Web3('http://127.0.0.1:7545')
 const fs = require('fs');
 const solc = require('solc');
 let datetime = new Date();
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser')
 const session =  require('express-session') ;
 const app = express();
 var contract = require("truffle-contract");
+var renderresult =false;
 app.use(bodyParser());
 app.use(express.static('public'));
 app.set('view engine','ejs');
@@ -17,19 +18,23 @@ const MongoClient = require('mongodb').MongoClient
 MongoClient.connect('mongodb+srv://vS12:npm123@cluster0-9p4gv.mongodb.net/Users?retryWrites=true',(err, database) =>{
 if (err) return console.log(err)
 AirlineDb = database.db('AirlinesConsortium')
+AirlineDb.collection('Airlines').deleteMany({})
+AirlineDb.collection('AirlineSeatsCount').deleteMany({})
+AirlineDb.collection('ChangeRequests').deleteMany({})
+AirlineDb.collection('ConsortiumRequests').deleteMany({})
+AirlineDb.collection('ConsortiumResponses').deleteMany({})
 app.listen(3000,function (){
 })
 })
 web3.eth.getAccounts().then(
-function(result){
-var chairPerson = result[0];
-var jetAir = result[1];
-var spiceAir = result[2];
-var contractAddress;
+function(addressObj){
+var airlinesDropDown;
+var i=0
+var chairPerson = addressObj[0];
+i++;
 var returnHash;
 var airlineName;
 var bookingId;
-
 
 const input = fs.readFileSync('Migrations.sol');
 const output = solc.compile(input.toString(), 1);
@@ -47,13 +52,9 @@ contract.deploy({
     gasPrice: '2000000000000'
 })
 .on('receipt', (receipt) => {
-   console.log("ok")
    add=receipt.contractAddress;
-   // console.log(receipt.contractAddress) // contains the new contract address
 
 })
-// var contract1 = new web3.eth.Contract(abi,add);
-// console.log(contractAddress)
 
 app.get('/', (req, res) => {
   AirlineDb.collection('Users').find().toArray(function(err, results){
@@ -84,7 +85,7 @@ app.get('/airlinerequests', (req, res) => {
 app.get('/userBookings', (req, res) => {
   var query = { UserName:loggedInUser};
   AirlineDb.collection('BookingFromUsers').find(query).toArray(function(err, results){
-      res.render('userBookings.ejs',{result:results});
+      res.render('userBookings.ejs',{result:results,airlinesDropDownejs:airlinesDropDown});
   })
 })
 
@@ -105,18 +106,16 @@ app.get('/Register', (req, res) => {
 
 
 app.post('/TransferRequest',(req, res)=>{
-  var requestedTo ;
-  if(airlineName == 'SpiceAir') 
-    requestedTo = 'JetAir';
-  else
-    requestedTo = 'SpiceAir';    
-  var airlineAddress;
-   
-   if(airlineName == 'SpiceAir')
-   {
-    var ok;
-      airlineAddress = spiceAir;
-      contract.methods.registerRequest(airlineAddress,jetAir,req.body.inputBookingId).send({from:airlineAddress}).on('transactionHash', (hashResult) => {
+    query = {_id:ObjectId(req.body.inputBookingId)};
+      AirlineDb.collection('ChangeRequests').find(query).toArray(function(err, changeRequestResults){
+      var fromAddress = changeRequestResults[0].CurrentAirline;
+      var toAddress = changeRequestResults[0].RequestedAirline;
+      var requestedTo = toAddress
+      query = {AirlineName :{$in:[fromAddress,toAddress]}}
+      AirlineDb.collection('Airlines').find(query).toArray(function(err, airlinesResults){
+      fromAddress = airlinesResults[0].airlineAddress;
+      toAddress = airlinesResults[1].airlineAddress;  
+      contract.methods.registerRequest(fromAddress,toAddress,req.body.inputBookingId).send({from:fromAddress}).on('transactionHash', (hashResult) => {
       var newTransferRequest = {
       BookingId : req.body.inputBookingId,
       RequestedByAirline : airlineName,
@@ -130,25 +129,8 @@ app.post('/TransferRequest',(req, res)=>{
               return res.redirect('/airlinerequests');
       })
       })
-   }
-   if(airlineName == 'JetAir')
-   {
-      airlineAddress = jetAir;
-      contract.methods.registerRequest(airlineAddress,spiceAir,req.body.inputBookingId).send({from:airlineAddress}).on('transactionHash', (hashResult) => {
-      var newTransferRequest = {
-      BookingId : req.body.inputBookingId,
-      RequestedByAirline : airlineName,
-      RequestedToAirline : requestedTo,
-      Createdtime:datetime, 
-      Status : "Pending",
-      TransactionId :hashResult,
-      ResponseId :""
-    };
-      AirlineDb.collection('ConsortiumRequests').save(newTransferRequest,(err, result) =>{
-      res.redirect('/airlinerequests');
-      })
-      })
-    }
+   })
+    })
 })
 
 app.post('/airlinesLogin',(req, res)=>{
@@ -158,7 +140,7 @@ app.post('/airlinesLogin',(req, res)=>{
    AirlineDb.collection("Airlines").find(query).toArray(function(err, resu) {
       airlineName = resu[0].AirlineName;
 
-    query = {CurrentAirline : airlineName};
+    query = {CurrentAirline : airlineName,Status:"Pending"};
     AirlineDb.collection('ChangeRequests').find(query).toArray(function(err, results){
       res.render('airlinePage.ejs',{result:results});
     })
@@ -166,6 +148,11 @@ app.post('/airlinesLogin',(req, res)=>{
 })
 
 app.post('/userlogin',(req, res)=>{
+  
+  AirlineDb.collection("Airlines").find().toArray(function(err,resu) {
+    airlinesDropDown = resu;
+
+  
    var returnValue;
    var inputpassword = req.body.logininputPassword;
    var query = { Name:req.body.logininputEmail};
@@ -175,15 +162,18 @@ app.post('/userlogin',(req, res)=>{
     AirlineDb.collection("BookingFromUsers").find(query).toArray(function(err, resu) {
      try 
      {
+
         loggedInUser = req.body.logininputEmail;
-        res.render('userBookings.ejs',{result:resu});
+
+        res.render('userBookings.ejs',{result:resu,airlinesDropDownejs:airlinesDropDown});
      }
      catch(ex)
      {
      }
     })
-  }
 
+  }
+});
 })
 })
 
@@ -195,21 +185,16 @@ app.post('/changeRequest',(req, res)=>{
    if(resu[0]._id == req.body.inputBookingId) {
     var obj = resu;
     AirlineDb.collection("BookingFromUsers").updateOne(query,newvalues,function(err, resu) {
-     try 
-     {
-     }
-     catch(ex)
-     {
-     }
+     try {}
+     catch(ex){}
     })
-
     //ChangeRequests
     var newChangeRequest = {
       BookingId : obj[0]._id,
-      CurrentAirline : obj[0].AirlineId,
+      CurrentAirline : obj[0].CurrentAirline,
       RequestedAirline : req.body.inputAirline,
       createdtime:datetime, 
-      status : "Pending",
+      Status : "Pending",
       PassengerId : obj[0].UserName
     };
       AirlineDb.collection('ChangeRequests').save(newChangeRequest,(err, result) =>{    
@@ -220,79 +205,73 @@ app.post('/changeRequest',(req, res)=>{
 })
 
 app.post('/Register',(req, res)=>{
-  const hash = bcrypt.hashSync(req.body.inputPassword,10);
-   var newAirline = {
-      AirlineId: req.body.inputEmail,
-      password: hash,
-      AirlineName:req.body.inputAirlineName,
-      TotalSeats:req.body.inputNumberOfSeats,
-      Source:req.body.inputFlyingFrom,
-      Destination:req.body.inputFlyingTo,
-      createdtime:datetime,
-      TransactionId:""
-   }; 
-
-   var airlineAddress;
-   
-   if(req.body.inputAirlineName == 'SpiceAir')
-   {
-      airlineAddress = spiceAir;
-      contract.methods.registerAirline(airlineAddress).send({from:airlineAddress}).on('transactionHash', (hashResult) => {
-      var query = {AirlineName : req.body.inputAirlineName}
-      var newvalues = { $set: { TransactionId: hashResult } };
-      AirlineDb.collection("Airlines").updateOne(query,newvalues,function(err, resu) {
-      res.redirect('/Register');
-      });
-    });
-   }
-   if(req.body.inputAirlineName == 'JetAir')
-   {
-      airlineAddress = jetAir;
-      contract.methods.registerAirline(airlineAddress).send({from:airlineAddress}).on('transactionHash', (hashResult) => {
-      var query = {AirlineName : req.body.inputAirlineName}
-      var newvalues = { $set: { TransactionId: hashResult } };
-      AirlineDb.collection("Airlines").updateOne(query,newvalues,function(err, resu) {
-      res.redirect('/Register');
-      });
-     });
-    }
+      var hash = bcrypt.hashSync(req.body.inputPassword,10);
+      airlineAddress = addressObj[i];
+      query ={AirlineName:req.body.inputAirlineName}; 
+      AirlineDb.collection("Airlines").find(query).toArray(function(err, result) {
+      if(result == "")
+      {
+        i++;
+          contract.methods.registerAirline(airlineAddress).send({from:airlineAddress}).on('transactionHash', (hashResult) => {
+          var newAirline = {
+            AirlineId: req.body.inputEmail,
+            password: hash,
+            AirlineName:req.body.inputAirlineName,
+            TotalSeats:req.body.inputNumberOfSeats,
+            Source:req.body.inputFlyingFrom,
+            Destination:req.body.inputFlyingTo,
+            createdtime:datetime,
+            TransactionId:hashResult,
+            airlineAddress:airlineAddress
+          };  
+          AirlineDb.collection('Airlines').save(newAirline,(result) =>{
+          var newAirlineSeatObj = {
+            AirlineName: req.body.inputAirlineName,
+            SeatsRemaining:req.body.inputNumberOfSeats,
+          };
+          AirlineDb.collection('AirlineSeatsCount').save(newAirlineSeatObj,(result) =>{
+             res.redirect('/Register');
+          });  
+        });
+        })  
+      }
+      else
+      {
+        res.redirect('/Register');
+        //show validation message
+      }  
 })
-
+})
 
 app.post('/VerifyAndApprove',(req, res)=>{
 var bookingId = req.body.inputBookingId;
 var newvalues;
-var airlineAddress;
-var toAddress;
 var requestId;
 var responseId;
 var query = {BookingId:bookingId};
 AirlineDb.collection("ConsortiumRequests").find(query).toArray(function(err, result) {
+      var toAddress = result[0].RequestedByAirline;
+      var fromAddress = result[0].RequestedToAirline;
+      query = {AirlineName :{$in:[fromAddress,toAddress]}}
+      AirlineDb.collection('Airlines').find(query).toArray(function(err, airlinesResults){
+      toAddress = airlinesResults[0].airlineAddress;
+      fromAddress = airlinesResults[1].airlineAddress;  
 requestId = result[0]._id;  
 query = {AirlineName:airlineName};
 AirlineDb.collection("AirlineSeatsCount").find(query).toArray(function(err, result) {
 if(result[0].SeatsRemaining > 1)
 {
-  query = {BookingId:bookingId};
+  query = {_id:ObjectId(bookingId)};
   newvalues = { $set: { Status: "Confirmed" } };
   AirlineDb.collection("BookingFromUsers").updateOne(query,newvalues,function(err, result) {
   query = {_id:ObjectId(bookingId)};
-  newvalues = { $set: { status: "Confirmed" } };
+  newvalues = { $set: { Status: "Confirmed" } };
   AirlineDb.collection("ChangeRequests").updateOne(query,newvalues,function(err, result) {
   AirlineDb.collection("ChangeRequests").find(query).toArray(function(err, result) {
   query = {_id:ObjectId(result[0].BookingId)};
-  newvalues = { $set: { Status: "Confirmed",AirlineId:airlineName } };  
+  newvalues = { $set: { Status: "Confirmed",CurrentAirline:airlineName } };  
   AirlineDb.collection("BookingFromUsers").updateOne(query,newvalues,function(err, result) {
-  if(airlineName == 'SpiceAir')  {
-    airlineAddress = spiceAir;
-    toAddress = jetAir;
-  }
-  else{
-    airlineAddress = jetAir;
-    toAddress = spiceAir;
-  }
-
-  contract.methods.registerResponse(airlineAddress,toAddress,bookingId,"Confirmed").send({from:airlineAddress}).on('transactionHash', (hashResult) => {
+  contract.methods.registerResponse(fromAddress,toAddress,bookingId,"Confirmed").send({from:fromAddress}).on('transactionHash', (hashResult) => {
 
   var newResponse = {
     RequestId : requestId,
@@ -311,8 +290,7 @@ if(result[0].SeatsRemaining > 1)
       AirlineDb.collection("AirlineSeatsCount").find(queryseat).toArray(function(err, result) {
       var newvaluesSeat = { $set: { SeatsRemaining:result[0].SeatsRemaining-1 }};     
       AirlineDb.collection("AirlineSeatsCount").updateOne(queryseat,newvaluesSeat,function(err, result) {
-      web3.eth.sendTransaction({from: airlineAddress,to : toAddress,value : web3.utils.toWei('1', 'ether')}, function(error, hash){
-
+      web3.eth.sendTransaction({from: fromAddress,to : toAddress,value : web3.utils.toWei('1', 'ether')}, function(error, hash){
       });
       res.redirect('/airlinerequests');
 })
@@ -325,12 +303,12 @@ if(result[0].SeatsRemaining > 1)
 })
 })
 })
-
 }
 else
 {
   //reject and change the status
 }
+})
 })
 })
 })
